f676f09c610e13fdc5331c191af194f3febfd691
Vladisl0ve <vladislav.lapko@gmail.com>
2024-06-22 03:27:19 +0200

    Added TickerVaverka

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Application } from 'pixi.js'
 import { VaverkaScene } from './scenes/VaverkaScene'; // This is the import statement
+import { TickerVaverka } from './scenes/TickerVaverka';
 
 const app = new Application<HTMLCanvasElement>({
     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -12,5 +13,7 @@ const app = new Application<HTMLCanvasElement>({
 
 // pass in the screen size to avoid "asking up"
 const sceny: VaverkaScene = new VaverkaScene();
+const sceny2: TickerVaverka = new TickerVaverka(app.screen.width, app.screen.height);
 
 app.stage.addChild(sceny)
+app.stage.addChild(sceny2)
